c7a3129737f74400600f8d26864abf2e2b24b7e0
DSCL-CHO <wptjcks15@gmail.com>
2025-09-11 17:32:22 +0900

    code(SQL-IPDL/kd_then_kp/coord_to_xyz.py)

--- SQL-IPDC/kd_then_kp/coord_to_xyz.py
+++ SQL-IPDC/kd_then_kp/coord_to_xyz.py
@@ -1,10 +1,6 @@
 # from gridworld import GridWorld 
 def coord_to_xyz(coord, grid_shape=(4, 4), origin=(0.3, -0.1, 0.2), cell_size=0.1):
 # def coord_to_xyz(coord, env, origin=(0.3, -0.1, 0.2), cell_size=0.1):
-    """
-    - origin: 그리드의 좌상단이 매핑되는 실제 위치
-    - cell_size: 각 셀 간 거리 (단위: m)
-    """
     row, col = coord
     # rows, cols = env.shape
     # rows, cols = grid_shape
